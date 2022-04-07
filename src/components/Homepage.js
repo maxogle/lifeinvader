926de88cae275ce4b1048bd1bc0cbdf5cf61f523
alexandermaxwellogle <maxogle@gmail.com>
2022-04-07 18:01:57 -0400

    added posts request

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,11 @@
 import Timeline from "./Timeline"
 import Sidebar from "./Sidebar"
+//https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json  michaels mock JSON
+
+
+
+
+
 
 
 const Homepage = () => {
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react'
+
+
+
+
+
+
 const Timeline = () => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(async () => {
+        console.log('Rendering Timeline....')
+        let req = await fetch('https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json')
+        let res = await req.json()
+        setPosts(res)
+    }, [])
+
     return (
-        <div></div>
+        <div>
+            <h2>Timeline</h2>
+        </div>
     )
 }
 
