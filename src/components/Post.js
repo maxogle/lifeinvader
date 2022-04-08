5d5006f67d7ab033008d55e2b48b1e94ba62aed2
alexandermaxwellogle <maxogle@gmail.com>
2022-04-08 14:29:09 -0400

    added heart glyph

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,7 +6,9 @@ const Post = ({post}) => {
         <div>
         <img src={post.image} height="250" width="250"/>
         <p>Posted by {post.username} ♥︎ {likes} likes</p>
-        <button onClick={() => { setLikes(likes + 1)} }>like</button>
+        <button onClick={() => { setLikes(likes + 1)} }>like
+            <i class="fa-solid fa-heart"></i>
+        </button>
         <p>{post.content}</p>
     </div>
     )
