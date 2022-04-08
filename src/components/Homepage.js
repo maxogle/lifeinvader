9b6e36fb08203341f1cfef31af7c0f478faa9c35
alexandermaxwellogle <maxogle@gmail.com>
2022-04-08 14:34:38 -0400

    added navbar

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,6 @@
 import Timeline from "./Timeline"
 import Sidebar from "./Sidebar"
+import NavBar from "./NavBar"
 //https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json michaels mock JSON
 
 
@@ -11,7 +12,7 @@ import Sidebar from "./Sidebar"
 const Homepage = () => {
     return(
         <div>
-            
+            <NavBar />
             <Timeline />
             <Sidebar />
         </div>
---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+const NavBar = () => {
+    return(
+        <nav className="navbar">
+            <div>
+                <a href="#">Lifeinvader</a>
+            </div>
+            <div>
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input type="search placeholder="search />"
+            </div>
+            <div>
+                <i class="fa-solid fa-bell"></i>
+            </div>
+        </nav>
+        
+    )
+}
+
+export default NavBar
